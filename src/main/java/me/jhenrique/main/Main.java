fbf64725eb36e2bb48a6732890922bf74560aab2
Mohammed Ezzat <mohamed.ezzat@devfactory.com>
2016-03-15 18:12:38 +0200

    squid:S1192 - String literals should not be duplicated

--- src/main/java/me/jhenrique/main/Main.java
+++ src/main/java/me/jhenrique/main/Main.java
@@ -5,7 +5,12 @@ import me.jhenrique.manager.TwitterCriteria;
 import me.jhenrique.model.Tweet;
 
 public class Main {
-	
+	private static final String USERNAME = "Username: ";
+	private static final String RETWEETS = "Retweets: ";
+	private static final String TEXT = "Text: ";
+	private static final String MENTIONS = "Mentions: ";
+	private static final String HASHTAGS = "Hashtags: ";
+
 	public static void main(String[] args) {
 		/**
 		 * Reusable objects
@@ -23,11 +28,11 @@ public class Main {
 		t = TweetManager.getTweets(criteria).get(0);
 		
 		System.out.println("### Example 1 - Get tweets by username [barackobama]");
-		System.out.println("Username: " + t.getUsername());
-		System.out.println("Retweets: " + t.getRetweets());
-		System.out.println("Text: " + t.getText());
-		System.out.println("Mentions: " + t.getMentions());
-		System.out.println("Hashtags: " + t.getHashtags());
+		System.out.println(USERNAME + t.getUsername());
+		System.out.println(RETWEETS + t.getRetweets());
+		System.out.println(TEXT + t.getText());
+		System.out.println(MENTIONS + t.getMentions());
+		System.out.println(HASHTAGS + t.getHashtags());
 		System.out.println();
 		
 		/**
@@ -41,11 +46,11 @@ public class Main {
 		t = TweetManager.getTweets(criteria).get(0);
 		
 		System.out.println("### Example 2 - Get tweets by query search [europe refugees]");
-		System.out.println("Username: " + t.getUsername());
-		System.out.println("Retweets: " + t.getRetweets());
-		System.out.println("Text: " + t.getText());
-		System.out.println("Mentions: " + t.getMentions());
-		System.out.println("Hashtags: " + t.getHashtags());
+		System.out.println(USERNAME + t.getUsername());
+		System.out.println(RETWEETS + t.getRetweets());
+		System.out.println(TEXT + t.getText());
+		System.out.println(MENTIONS + t.getMentions());
+		System.out.println(HASHTAGS + t.getHashtags());
 		System.out.println();
 		
 		/**
@@ -59,11 +64,11 @@ public class Main {
 		t = TweetManager.getTweets(criteria).get(0);
 		
 		System.out.println("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']");
-		System.out.println("Username: " + t.getUsername());
-		System.out.println("Retweets: " + t.getRetweets());
-		System.out.println("Text: " + t.getText());
-		System.out.println("Mentions: " + t.getMentions());
-		System.out.println("Hashtags: " + t.getHashtags());
+		System.out.println(USERNAME + t.getUsername());
+		System.out.println(RETWEETS + t.getRetweets());
+		System.out.println(TEXT + t.getText());
+		System.out.println(MENTIONS + t.getMentions());
+		System.out.println(HASHTAGS + t.getHashtags());
 		System.out.println();
 	}
 	
